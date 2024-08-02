71e336755561779ca2ed9bf327fb59d2abd64838
khs00353 <khs00353@gmail.com>
2024-08-02 16:38:04 +0900

    .env 파일 추가

--- src/main/java/com/energy/welfare/Application.java
+++ src/main/java/com/energy/welfare/Application.java
@@ -1,12 +1,20 @@
 package com.energy.welfare;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
 }
